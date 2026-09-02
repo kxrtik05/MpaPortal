149cb885b185bdc9b92586c833c2d1f4ff677188
kxrtik05 <kartiknaik2005@gmail.com>
2026-09-02 12:51:21 +0530

    Enhance CORS configuration and update server listen address

--- backend/server.js
+++ backend/server.js
@@ -29,7 +29,19 @@ connectDB();
 // MIDDLEWARE
 // ===============================
 
-app.use(cors());
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "http://localhost:5174",
+      "http://localhost:5175",
+      "https://mpa-complaint-portal.vercel.app",
+    ],
+    credentials: true,
+  })
+);
+
+
 app.use(express.json());
 
 // ===============================
@@ -58,6 +70,6 @@ app.get("/", (req, res) => {
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
   console.log(`🚀 Server running on port ${PORT}`);
 });
